173e9d15830292f7f29725b44227124670966c40
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-10 20:06:31 -0300

    feat: add movie routes

---
+++ src/routes/movies-routes.php
@@ -0,0 +1,15 @@
+<?php
+class MoviesRoutes{
+    private $controller;
+    private $royte;
+
+    public function __construct($controller, $route){
+        $this->controller = $controller;
+        $this->route = $route;
+    }
+
+    router(){
+        //add "page navigation" routes with js
+    }
+}
+?>
