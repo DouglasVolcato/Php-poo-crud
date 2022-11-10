5844928356257ad0f8cef1a67be809eaa490ccc5
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-10 13:28:10 -0300

    refactor: movie service exports to a unique file

---
+++ src/services/index.php
@@ -0,0 +1,7 @@
+<?php
+require("create-movie-service.php");
+require("delete-movie-service.php");
+require("getall-movie-service.php");
+require("getbyid-movie-service.php");
+require("update-movie-service.php");
+?>
