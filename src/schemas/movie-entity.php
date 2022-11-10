d8b7c097546083c5de9ff795ccd7377f7053e57a
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-10 00:12:28 -0300

    feat: add movie entity with validation

---
+++ src/schemas/movie-entity.php
@@ -0,0 +1,27 @@
+<?php
+class MovieEntity{
+    private $title;
+    private $year;
+    private $image;
+
+    public function __construct($title, $year, $image){
+        $this->title = $title;
+        $this->year = $year;
+        $this->image = $image;
+    }
+
+    public function validate(){
+        if($this->$title){
+            throw new Exception("Movie must have a title.");
+        }
+    }
+
+    public function getMovie(){
+        $movie = new stdClass;
+        $movie->title = $this->title;
+        $movie->year = $this->year ? $this->year : 0;
+        $movie->image = $this->image ? $this->image : "";
+        return $movie;
+    }
+};
+?>
