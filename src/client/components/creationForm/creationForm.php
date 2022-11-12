68622b7d79e60f71a5fd463eb004e04e9eae4a0b
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-12 01:22:01 -0300

    feat: add form for movie creation

---
+++ src/client/components/creationForm/creationForm.php
@@ -0,0 +1,14 @@
+<?php
+require("./src/client/routes/createMovie-route.php");
+
+function creationForm(){
+    echo '
+    <form class="creationForm" method="post" action="">
+        <input class="creationForm-input" type="text" name="title" placeholder="Title" required />
+        <input class="creationForm-input" type="number" name="year" placeholder="Year" required />
+        <input class="creationForm-input" type="text" name="image" placeholder="Image" required />
+        <button class="creationForm-button" type="submit">Add Movie</button>
+    </form>
+    ';
+}
+?>
