5439cc2c2f0601bd374027083c9499d31665169f
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-12 12:45:41 -0300

    refactor: move head to a head component

--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./src/client/styles/index.css">
-    <link rel="stylesheet" href="./src/client/components/creationForm/creationForm.css">
-    <link rel="stylesheet" href="./src/client/components/title/title.css">
-    <link rel="stylesheet" href="./src/client/components/moviesBody/moviesBody.css">
-    <link rel="stylesheet" href="./src/client/components/movieCard/movieCard.css">
-    <link rel="stylesheet" href="./src/client/components/footer/footer.css">
-    <title>Movies</title>
-</head>
 <?php
+    require_once('./src/client/components/head/head.php');
     require_once('./src/client/pages/home.php');
+    require_once('./src/client/utils/echoComponents.php');
+
+    echoComponents([
+        head(),
+        homePage()
+    ]);
 ?>
 </html>
---
+++ src/client/components/head/head.php
@@ -0,0 +1,17 @@
+<?php
+function head(){
+    return '
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./src/client/styles/index.css">
+        <link rel="stylesheet" href="./src/client/components/creationForm/creationForm.css">
+        <link rel="stylesheet" href="./src/client/components/title/title.css">
+        <link rel="stylesheet" href="./src/client/components/moviesBody/moviesBody.css">
+        <link rel="stylesheet" href="./src/client/components/movieCard/movieCard.css">
+        <link rel="stylesheet" href="./src/client/components/footer/footer.css">
+        <title>Movies</title>
+    </head>';
+}
+?>
