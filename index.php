1123fae6d8b62a05d6576348b634d09439783296
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-12 19:00:12 -0300

    feat: add page redirect in index

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-    require_once('./src/client/components/head/head.php');
-    require_once('./src/client/pages/home.php');
-    require_once('./src/client/utils/echoComponents.php');
-
-    echoComponents([
-        head(),
-        homePage()
-    ]);
+    include("./src/client/pages/home.php");
 ?>
 </html>
