e0bd2932f218cb8d5c84731ffe666f4ad1a0e177
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-12 18:59:56 -0300

    feat: add stylesheets

--- src/client/components/head/head.php
+++ src/client/components/head/head.php
@@ -6,11 +6,14 @@ function head(){
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./src/client/styles/index.css">
-        <link rel="stylesheet" href="./src/client/components/creationForm/creationForm.css">
+        <link rel="stylesheet" href="./src/client/components/form/form.css">
         <link rel="stylesheet" href="./src/client/components/title/title.css">
-        <link rel="stylesheet" href="./src/client/components/moviesBody/moviesBody.css">
+        <link rel="stylesheet" href="./src/client/components/flexBody/flexBody.css">
         <link rel="stylesheet" href="./src/client/components/movieCard/movieCard.css">
         <link rel="stylesheet" href="./src/client/components/footer/footer.css">
+        <link rel="stylesheet" href="./src/client/components/button/button.css">
+        <link rel="stylesheet" href="./src/client/components/image/image.css">
+        <link rel="stylesheet" href="./src/client/components/modal/modal.css">
         <title>Movies</title>
     </head>';
 }
