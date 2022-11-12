805258b691c7207f6dc231f414a713b4c2762ea4
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-12 18:59:22 -0300

    feat: move image tag to a component

--- src/client/components/movieCard/movieCard.php
+++ src/client/components/movieCard/movieCard.php
@@ -1,11 +1,13 @@
 <?php
+require('./src/client/components/image/image.php');
+
 function movieCard($movie){
     return '
     <div class="movieCard">
         <p class="movieCard-text">Title: '.$movie['title'].'</p>
-        <p class="movieCard-text">Year: '.$movie['year'].'</p>
-        <img class="movieCard-image" src="'.$movie['image'].'"/>
-    </div>
+        <p class="movieCard-text">Year: '.$movie['year'].'</p>'.
+        image($movie['image'])
+    .'</div>
     ';
 }
 ?>
