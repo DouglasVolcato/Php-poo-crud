2d6a472c689c91cc12ac679778e507a7ae6bc620
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-12 21:53:52 -0300

    feat: add labels and actions to form component

--- src/client/components/form/form.php
+++ src/client/components/form/form.php
@@ -1,19 +1,22 @@
 <?php
 require('./src/client/components/button/button.php');
 
-function form($inputFieldNames, $inputFieldTypes, $inputFieldValues, $buttonNames, $buttonTypes){
+function form($action ,$inputFieldNames, $inputFieldTypes, $inputFieldValues, $buttonNames, $buttonTypes, $buttonClickFunctions){
     $inputs = "";
     for ($i = 0; $i < count($inputFieldNames); $i++) {
+        if($inputFieldTypes[$i] !== "hidden"){
+            $inputs = $inputs . " " . '<label class="form-label">'.ucfirst($inputFieldNames[$i]).'</label>';
+        }
         $inputs = $inputs . " " . '<input class="form-input" type="'.$inputFieldTypes[$i].'" name="'.$inputFieldNames[$i].'" value="'.$inputFieldValues[$i].'" placeholder="'.ucfirst($inputFieldNames[$i]).'" required />';
     }
 
     $buttons = "";
     for ($i = 0; $i < count($buttonNames); $i++) {
-        $buttons = $buttons . " " . button($buttonNames[$i], $buttonTypes[$i]);
+        $buttons = $buttons . " " . button($buttonNames[$i], $buttonTypes[$i], $buttonClickFunctions[$i], 'blue');
     }
 
     return '
-    <form class="form" method="post" action="">'.
+    <form class="form" method="post" action="'.$action.'">'.
     $inputs.
     $buttons.
     '</form>
