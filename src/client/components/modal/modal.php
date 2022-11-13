7eedd5dab30e1d208aaaa4080c8953fd7b45eecc
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-12 21:50:24 -0300

    refactor: remove unused code

--- src/client/components/modal/modal.php
+++
@@ -1,5 +0,0 @@
-<?php
-function modal($components){
-    return '<div class="modal"><div class="modal-inside">'.$components.'</div></div>';
-}
-?>
--- src/client/routes/createMovie-route.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('./src/server/factories/movie-factory.php');
-
-if(isset($_POST['title'])){
-    $created = makeMovieFactory("createMovie");
-}
-?>
