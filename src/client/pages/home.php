5c188a2a9a5cdd52ccd66888927c13502c496f54
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-12 22:01:00 -0300

    refactor: move movie card array separator to a unique component

--- src/client/pages/home.php
+++ src/client/pages/home.php
@@ -2,16 +2,18 @@
 require_once("./src/client/components/title/title.php");
 require_once("./src/client/components/form/form.php");
 require_once("./src/client/components/flexBody/flexBody.php");
-require_once("./src/client/components/footer/footer.php");
 require_once("./src/client/routes/getAllMovies.php");
 require_once('./src/client/components/head/head.php');
+require_once("./src/client/components/footer/footer.php");
 require_once('./src/client/utils/echoComponents.php');
+require_once('./src/client/routes/createMovie.php');
+require_once('./src/client/components/movieCards/movieCards.php');
 
     echoComponents([
         head(),
         title("Movies"),
-        form(['title', 'year', 'image'], ['text', 'number', 'text'], ['', '', ''], ['Add movie'], ['submit']),
-        flexBody(getAllMovies()),
+        form("", ['title', 'year', 'image'], ['text', 'number', 'text'], ['', '', ''], ['Add movie'], ['submit'], ['']),
+        flexBody(movieCards(getAllMovies())),
         footer("Made by Douglas Volcato"),
     ]);
 ?>
